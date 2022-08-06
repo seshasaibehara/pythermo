5d80fd6bee1d733b1eff7c9ca73ed94fbea2204b
seshasaibehara <seshasaibehara@gmail.com>
2022-08-05 21:50:08 -0700

    added documentation for casm-jobs

--- pythermo/jobs/casm_jobs.py
+++ pythermo/jobs/casm_jobs.py
@@ -6,17 +6,21 @@ import pymatgen.io.vasp as pmgvasp
 from pymatgen.io.cif import CifWriter
 
 
-def _get_file_paths_as_a_string(file_type, calctype_dirs):
-    """TODO: Docstring for _get_file_paths_as_a_string.
+def _get_file_paths_as_a_string(file_type: str, calctype_dirs: list[str]) -> str:
+    """Return file paths in casm configurations' calctype directory
+    filetype can be anything like INCAR, KPOINTS, POTCAR, etc
 
     Parameters
     ----------
-    file_type : TODO
-    calctype_dirs : TODO
+    file_type : str
+        file_type like INCAR, KPOINTS, POTCAR etc.
+    calctype_dirs : list[str]
+        list of all configurations calctype directories
 
     Returns
     -------
-    TODO
+    str
+        A long string with all the file paths
 
     """
     file_paths = [
@@ -29,32 +33,40 @@ def _get_file_paths_as_a_string(file_type, calctype_dirs):
     return file_path_str
 
 
-def _get_config_names(selected_configurations):
-    """TODO: Docstring for _get_config_names.
+def _get_config_names(selected_configurations: list[dict]) -> list[str]:
+    """Given a list of configurations in ccasm query json format,
+    return a list of config names example ["SCEL1_1_1_1_0_0_0/0"]
 
     Parameters
     ----------
-    selected_configurations : TODO
+    selected_configurations : list[dict]
+        list of configurations in ccasm query json format
 
     Returns
     -------
-    TODO
+    list[str]
+        List of confignames
 
     """
     return [config["name"] for config in selected_configurations]
 
 
-def _get_calctype_dirs(selected_configurations, calctype):
-    """TODO: Docstring for _get_calctype_dirs.
+def _get_calctype_dirs(selected_configurations: list[dict], calctype: str) -> list[str]:
+    """Given a list of configurations in ccasm query json format and calctype,
+    returns list of configurations' calctype directories
+    (training_data/configname/calctype.calctype)
 
     Parameters
     ----------
-    selected_configurations : TODO
-    calctype : TODO
+    selected_configurations : list[dict]
+        list of configurations in ccasm query json format
+    calctype : str
+        ccasm query calctype
 
     Returns
     -------
-    TODO
+    list[str]
+        list of configurations' calctype directories
 
     """
     return [
@@ -64,29 +76,40 @@ def _get_calctype_dirs(selected_configurations, calctype):
 
 
 def toss_file_str(
-    selected_configurations,
-    calctype="default",
-    write_incar=True,
-    write_poscar=True,
-    write_kpoints=True,
-    write_potcar=True,
-    write_relaxandstatic=True,
-):
-    """TODO: Docstring for write_toss_file.
+    selected_configurations: list[dict],
+    calctype: str = "default",
+    write_incar: str = True,
+    write_poscar: str = True,
+    write_kpoints: str = True,
+    write_potcar: str = True,
+    write_relaxandstatic: str = True,
+) -> str:
+    """For a given list of configurations in ccasm query json format,
+    return a string which can be used in conjuction with rsync --files-from
+    By default, contains configuration directories with INCAR, KPOINTS,
+    POSCAR, POTCAR, and relaxandstatic.sh
 
     Parameters
     ----------
-    selected_configurations : TODO
-    calctype : TODO, optional
-    write_incar : TODO, optional
-    write_poscar : TODO, optional
-    write_kpoints : TODO, optional
-    write_potcar : TODO, optional
-    write_relaxandstatic : TODO, optional
+    selected_configurations : list[dict]
+        list of configurations in ccasm query json format
+    calctype : str, optional
+        ccasm calctype
+    write_incar : bool, optional
+        whether to include incar (default=True)
+    write_poscar : bool, optional
+        whether to include poscar (default=True)
+    write_kpoints : bool, optional
+        whether to include kpoints (default=True)
+    write_potcar : bool, optional
+        whether to include potcar (default=True)
+    write_relaxandstatic : bool, optional
+        whether to include relaxandstatic.sh (default=True)
 
     Returns
     -------
-    TODO
+    str
+        When written to a file can be used with rsync --files-from
 
     """
     calctype_dirs = _get_calctype_dirs(selected_configurations, calctype)
@@ -106,18 +129,32 @@ def toss_file_str(
     return toss_file_str
 
 
-def submit_script_str(selected_configurations, queue_type: str, calctype="default"):
-    """TODO: Docstring for submit_script_str.
+def submit_script_str(
+    selected_configurations: list[dict], queue_type: str, calctype: str = "default"
+) -> str:
+    """Given a list of configurations in ccasm query json format and a queue type
+    (slurm or pbs), return a submit string which can be executed as bash script,
+    when written to a file to submit the calculations.
 
     Parameters
     ----------
-    selected_configurations : TODO
-    queue_type : TODO
-    calctype : TODO, optional
+    selected_configurations : list[dict]
+        configurations in ccasm query json format
+    queue_type : str
+        HPC queue type (slurm or pbs)
+    calctype : str, optional
+        ccasm calctype
 
     Returns
     -------
-    TODO
+    str
+        A string when written to a file can be executed as bash file
+        to submit calculations
+
+    Raises
+    ------
+    RuntimeError
+        When ``queue_type`` is neither pbs or slurm
 
     """
     calctype_dirs = _get_calctype_dirs(selected_configurations, calctype)
@@ -132,42 +169,58 @@ def submit_script_str(selected_configurations, queue_type: str, calctype="defaul
     submit_str += " cd $i\n"
     if queue_type == "pbs":
         submit_str += " qsub relaxandstatic.sh\n"
-    if queue_type == "slurm":
+    elif queue_type == "slurm":
         submit_str += " sbatch relaxandstatic.sh\n"
+    else:
+        raise RuntimeError("Invalid queue (" + queue_type + ")")
     submit_str += " cd ../../../../\n"
     submit_str += "done\n"
 
     return submit_str
 
 
-def _job_names(config_names):
-    """TODO: Docstring for _job_names.
+def _job_names(config_names: list[str]) -> list[str]:
+    """Given a list of configuration names, replace
+    "/" to "." which can be used a job name
 
     Parameters
     ----------
-    config_names : TODO
+    config_names : list[str]
+        list of configuration names
 
     Returns
     -------
-    TODO
+    list[str]
+        list of configuration names where "/" is replaced by "."
 
     """
     return [name.replace("/", ".") for name in config_names]
 
 
 def copy_relaxandstatic_cmds(
-    selected_configurations, relaxandstatic_path=None, calctype="default"
-):
-    """TODO: Docstring for copy_relaxandstatic_cmd.
+    selected_configurations: list[dict],
+    relaxandstatic_path: str = None,
+    calctype: str = "default",
+) -> list[str]:
+    """For a given list of configurations in ccasm query json format,
+    write out copy commands for relaxandstatic.sh file from training_data/givencalctype/settings/
+    (if an explicit path is not given using ``relaxandstatic_path``) to
+    each configuration's calctype folder
 
     Parameters
     ----------
-    selected_configurations : TODO
-    calctype : TODO, optional
+    selected_configurations : list[dict]
+        list of configurations in ccasm query json format
+    relaxandstatic_path : str, optional
+        path to relaxandstatic.sh (default=training_data/calctype.given/settings/relaxandstatic.sh
+    calctype : str, optional
+        ccasm calctype
 
     Returns
     -------
-    TODO
+    list[str]
+        List of copy commands to copy relaxandstatic.sh to configuration's calctype
+        directory
 
     """
     if relaxandstatic_path is None:
@@ -183,18 +236,33 @@ def copy_relaxandstatic_cmds(
     ]
 
 
-def change_job_names_cmds(selected_configurations, queue, calctype="default"):
-    """TODO: Docstring for change_job_names_cmds.
+def change_job_names_cmds(
+    selected_configurations: list[dict], queue: str, calctype: str = "default"
+) -> list[str]:
+    """List of sed commands to change job names for calculations in a given list of
+    selected configurations and calctype. If ``queue`` is ``slurm``, the sed
+    command will replace the ``#SBATCH -J``, line with job name as configname.
+    And if the ``queue`` is ``pbs``, ``#PBS -N``, line will be replaced.
 
     Parameters
     ----------
-    selected_configurations : TODO
-    queue : TODO
-    calctype : TODO, optional
+    selected_configurations : list[dict]
+        list of configurations in ccasm query json format
+    queue : str
+        cluster queue type, accepted arguments pbs or slurm
+    calctype : str, optional
+        ccasm calctype (default="default")
 
     Returns
     -------
-    TODO
+    list[str]
+        List of sed commands, when executed with os.system will change
+        job names in relaxandstatic.sh file
+
+    Raises
+    ------
+    RuntimeError
+        If a queue type provided is neither slurm or pbs
 
     """
     calctype_dirs = _get_calctype_dirs(selected_configurations, calctype)
@@ -217,21 +285,27 @@ def change_job_names_cmds(selected_configurations, queue, calctype="default"):
             for job_name, calctype_dir in zip(job_names, calctype_dirs)
         ]
     else:
-        raise ValueError(str(queue) + " is not a valid queue.")
+        raise RuntimeError(str(queue) + " is not a valid queue.")
 
 
-def write_incars(selected_configurations, incars, calctype):
-    """TODO: Docstring for write_incar_cmds.
+def write_incars(
+    selected_configurations: list[dict], incars: list[pmgvasp.Incar], calctype: str
+) -> None:
+    """Write pymatgen Incars for ``selected_configurations`` in a given ``calctype``
+    directory
 
     Parameters
     ----------
-    selected_configurations : TODO
-    incars : TODO
-    calctype : TODO
+    selected_configurations : list[dict]
+        list of configurations in ccasm query json format
+    incars : list[pmgvasp.Incar]
+        list of pymatgen Incars
+    calctype : str
+        ccasm calctype where you want to write INCARs
 
     Returns
     -------
-    TODO
+    None
 
     """
     calctype_dirs = _get_calctype_dirs(selected_configurations, calctype)
@@ -242,21 +316,40 @@ def write_incars(selected_configurations, incars, calctype):
     return None
 
 
+# TODO: Allow to change magnetic moments of each element individually
+# TODO: Allow for non-collinear calculations
 def modify_incar_magmoms(
-    selected_configurations, new_magmoms: dict, calctype="default", noncollinear=False
-):
-    """TODO: Docstring for modify_incar_magmoms.
+    selected_configurations: list[dict],
+    new_magmoms: dict,
+    calctype: str = "default",
+    noncollinear: bool = False,
+) -> list[pmgvasp.Incar]:
+    """For a given list of configurations in ccasm query json format,
+    this function returns a list of pymatgen INCARs with magnetic moments
+    modified using ``new_magmoms``. For example, if you want to modify
+    magnetic moments of Li, O (magmoms of Mn, Ni will be kept)
+    in Li2MnNiO8 structure:\n
+    Example ``new_magmoms``:\n
+    {\n
+        "atom_types": ["Li", "O"],\n
+        "magmoms" : [0.6, 0.4],\n
+    }\n
 
     Parameters
     ----------
-    selected_configurations : TODO
-    new_magmoms : TODO
-    calctype : TODO, optional
+    selected_configurations : list[dict]
+        List of configurations in ccasm query json format
+    new_magmoms : dict
+        dictionary containing new ``magmoms`` for given ``atom_types``
+    calctype : str, optional
+        ccasm calctype (default="default")
+    noncollinear : bool, optional
+        If the calculations are noncollinear (default=False)
 
     Returns
     -------
-    TODO
-
+    list[pymgvasp.Incar]
+        List of pymatgen Incars with modified magnetic moments
     """
     if noncollinear:
         raise NotImplementedError("NONCOLLINEAR calculations not yet implemented")
@@ -282,7 +375,8 @@ def modify_incar_magmoms(
         if "LNONCOLLINEAR" in incar_keys or "LSORBIT" in incar_keys:
             raise NotImplementedError("NONCOLLINEAR calculations not yet implemented")
 
-        # get the updated magnetic moments for each atom type (assumes each atom type has the same value of magmom)
+        # get the updated magnetic moments for each atom type
+        # (assumes each atom type has the same value of magmom)
         # if magenetic moment for an atom type is not provided, use the one already in incar
         modified_magmoms = []
         index = 0
@@ -306,18 +400,43 @@ def modify_incar_magmoms(
 
 def get_casm_commands_to_turn_off_given_configurations(
     configurations_info: dict, casm_executable="ccasm"
-):
-    """TODO: Docstring for get_casm_commands_to_turn_off_given_configurations.
+) -> list[str]:
+    """Given ``configurations_info``, return a list of ccasm commands to
+    turn them off. \n
+
+    Example ``configurations_info``:\n
+    {\n
+     "scelnames" : ["SCEL1..", "SCEL2...", ....]\n
+     "range": [{"start":0, "stop":4}, [1, 3, 4,], ....]\n
+    }\n
+
+    "scelnames" is a list containing ccasm supercell names for which configuration list will
+    be constructed. "range" is a list with each element corresponding to configurations in
+    a supercell corresponding to the same index in "scelnames". The configurations can either
+    be given as a dictionary with "start" and "stop" numbers (stop is included) or just a
+    list of numbers
 
     Parameters
     ----------
-    configurations_info : TODO
+    configurations_info : dict
+        Read the example above to understand how it's interpreted.
 
     Returns
     -------
-    TODO
+    list[str]
+        A list of ccasm commands which can be executed to turn off
+        given configurations
+
+    Raises
+    ------
+    RuntimeError
+        If length of ``configurations_info["scelnames"]`` doesn't match
+        with length of ``configurations_info["range"]``
 
     """
+    if len(configurations_info["scelnames"]) != len(configurations_info["range"]):
+        raise RuntimeError("length of scelnames doesn't match with length of range")
+
     scels = configurations_info["scelnames"]
 
     config_range_for_scel = [
@@ -422,3 +541,48 @@ def visualize_magnetic_moments_from_casm_structure(
     ]
 
     return mcifs
+
+
+def remove_completed_calculations(
+    selected_configurations: list[dict], calctype: str = "default"
+) -> str:
+    """For a given list of configurations (in ccasm query json format),
+    a status.json file will be read from training_data/configname/calctype/status.json.
+    If status["status"] is "complete", a line will be added to the output as
+    "rm -rf training_data/configname/calctype"
+
+    WARNING: USE THIS STRING TO WRITE A BASH FILE AND EXECUTE IT ONLY IF YOU WANT
+    TO REMOVE COMPLETED CALCULATIONS ON A REMOTE CLUSTER. MAKE SURE THE DATA IS
+    SYNCED TO YOUR COMPUTER. DON'T USE IT ON YOUR OWN COMPUTER.
+
+    Parameters
+    ----------
+    selected_configurations : list[dict]
+        List of configurations in ccasm query json format
+    calctype : str, optional
+        ccasm ``calctype`` of your DFT calcuations
+
+    Returns
+    -------
+    str
+        Write this string to a bash file and execute it to remove completed
+        calculations
+
+    """
+    calctype_dirs = _get_calctype_dirs(selected_configurations, calctype)
+
+    # read status files
+    # if status is complete, add the corresponding config dir to be removed
+    remove_str = "#!/bin/bash\n"
+    remove_str += "#WARNING: USE THIS FILE ONLY TO REMOVE COMPLETED CALCULATIONS\n"
+    remove_str += (
+        "#WARNING: USE IT ON A REMOTE CLUSTER AFTER THE CALCULATIONS ARE SYNCED\n"
+    )
+    for calctype_dir in calctype_dirs:
+        with open(os.path.join(calctype_dir, "status.json"), "r") as f:
+            status = json.load(f)
+
+        if status["status"] == "complete":
+            remove_str += "rm -rf " + str(calctype_dir) + "/\n"
+
+    return remove_str
